1328c2c2fa5ad8a549cde33702f7d8d1615668c3
yabby1997 <yabby1997@gmail.com>
2020-06-20 12:27:10 +0900

    App Development with Swift의 계산기 예제를 작동하도록 구현해보기

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,7 +14,92 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet var displayLabel: UILabel!
+    var operatorAddable = true
+    
+    @IBAction func allClear(_ sender : Any){
+        displayLabel.text = ""
+        operatorAddable = true
+    }
+    
+    @IBAction func oneTapped(_ sender : Any){
+        displayLabel.text! += "1"
+        operatorAddable = true
+    }
+    
+    @IBAction func twoTapped(_ seender : Any){
+        displayLabel.text! += "2"
+        operatorAddable = true
+    }
+    
+    @IBAction func threeTapped(_ seender : Any){
+        displayLabel.text! += "3"
+        operatorAddable = true
+    }
+    
+    @IBAction func fourTapped(_ seender : Any){
+        displayLabel.text! += "4"
+        operatorAddable = true
+    }
+    
+    @IBAction func fiveTapped(_ seender : Any){
+        displayLabel.text! += "5"
+        operatorAddable = true
+    }
+    
+    @IBAction func sixTapped(_ seender : Any){
+        displayLabel.text! += "6"
+        operatorAddable = true
+    }
+    
+    @IBAction func sevenTapped(_ seender : Any){
+        displayLabel.text! += "7"
+        operatorAddable = true
+    }
+    
+    @IBAction func eightTapped(_ seender : Any){
+        displayLabel.text! += "8"
+        operatorAddable = true
+    }
+    
+    @IBAction func nineTapped(_ seender : Any){
+        displayLabel.text! += "9"
+        operatorAddable = true
+    }
+    
+    @IBAction func zeroTapped(_ seender : Any){
+        displayLabel.text! += "0"
+        operatorAddable = true
+    }
+    
+    @IBAction func plusTapped(_ sender : Any){
+        if operatorAddable && displayLabel.text! != ""{
+            displayLabel.text! += "+"
+            operatorAddable = false
+        }
+    }
+    
+    @IBAction func minusTapped(_ sender : Any){
+        if operatorAddable && displayLabel.text! != ""{
+            displayLabel.text! += "-"
+            operatorAddable = false
+        }
+    }
+    
+    @IBAction func multiplyTapped(_ sender : Any){
+        if operatorAddable && displayLabel.text! != ""{
+            displayLabel.text! += "x"
+            operatorAddable = false
+        }
+    }
+    
+    @IBAction func divideTapped(_ sender : Any){
+        if operatorAddable && displayLabel.text! != ""{
+            displayLabel.text! += "/"
+            operatorAddable = false
+        }
+    }
+    
 }
 
